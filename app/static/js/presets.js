1a7484e247e8c3a9bf34b96d6a988fcdb66feb9f
Jake B <jacobboxerman@gmail.com>
2021-06-13 21:41:34 -0500

    Added presets, return button

---
+++ app/static/js/presets.js
@@ -0,0 +1,21 @@
+function challahFill() {
+	document.getElementById("mixing_time").value = 30
+	document.getElementById("rise0").value = 180
+	document.getElementById("first_rise_range").value = 30
+	document.getElementById("rise1").value = 90
+	document.getElementById("action_time").value = 15
+	document.getElementById("bake_time").value = 35
+	document.getElementById("cool_time").value = 15
+	document.getElementById("bread_kind").value = "Challah"
+}
+
+function clearFill() {
+	document.getElementById("mixing_time").value = 
+	document.getElementById("rise0").value = 
+	document.getElementById("first_rise_range").value = 
+	document.getElementById("rise1").value = 
+	document.getElementById("action_time").value = 
+	document.getElementById("bake_time").value = 
+	document.getElementById("cool_time").value = 
+	document.getElementById("bread_kind").value = ""
+}
